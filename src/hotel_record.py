173f5155ff734ed506921cb81b40d7aa6353c8e5
KJHJason <kuanjunhaojason@gmail.com>
2022-05-15 18:43:05 +0800

    shortened code

--- src/hotel_record.py
+++ src/hotel_record.py
@@ -631,11 +631,11 @@ X. Exit
         Worst time complexity: O(n)
         Average time complexity: O(n)
         """
+        get_data = lambda d: d.get_customer_name() if (typeOfSearch == "customer") \
+                                                   else d.get_package_name()
         arr = []
         for record in self.__db:
-            if (typeOfSearch == "customer" and record.get_customer_name() == target):
-                arr.append(record)
-            elif (typeOfSearch == "package" and record.get_package_name() == target):
+            if (get_data(record) == target):
                 arr.append(record)
         return -1 if (len(arr) == 0) else arr
 
